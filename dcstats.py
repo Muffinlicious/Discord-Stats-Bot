23cb144c9fd173b6aae6b9cbc2de45648a3ce7c2
Muffinlicious <alicornderpy@gmail.com>
2019-06-25 23:35:22 -0700

    Update dcstats.py

--- dcstats.py
+++ dcstats.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy
 
 RE_CUSTOM_EMOJI = r'<a?(:[^\s:]+:)\d{1,20}>'
+# ^ Captures :cathug-3: from the string '<a:cathug-3:443111261899718658>'
 
 with open('swears.txt') as f:
   SWEARS = f.read().lower().split('\n')
@@ -18,19 +19,21 @@ def _autotallydict(dictionary, key):
 
 def hascurse(string):
   for curse in SWEARS:
-    if curse in string:
+    if curse in string.lower():
       return True
   return False
 
 def getemojis(string):
   emojis = list()
   for d in emoji_lis(string):
-    emojis.append(d['emoji'])
+    emojis.append(demojize(d['emoji']))
   for e in findall(RE_CUSTOM_EMOJI, string):
     emojis.append(e)
+  return emojis
 
 class _MsgStatistics:
   def __init__(self):
+    self.msgs = 0
     self.curses = 0
     self.chars = 0
     self.words = 0
@@ -43,23 +46,29 @@ class _MsgStatistics:
     
     self.hourslots = {h:0 for h in range(24)}
     self.monthslots = dict()
+    
+  @property
   def curseratio(self):
-    return self.curses / self.msgs
-  
+    return float('%.3f' % (self.curses / self.msgs))
+  @property
   def charratio(self):
-    return self.chars / self.msgs
-
+    return float('%.2f' % (self.chars / self.msgs))
+  @property
   def wordratio(self):
-    return self.words / self.msgs
+    return float('%.2f' % (self.words / self.msgs))
   
   def feed(self, message):
+    # Check if message is a duplicate
+    if message.id in self.idlist:
+      raise ValueError("Message has already been logged")
+    self.msgs += 1
     self.idlist.append(message.id)
     # Cursing
     if hascurse(message.content):
       self.curses += 1
     # Mentions (Pings)
     for user in message.mentions:
-      self.pings = _autotallydict(self.pings, user)
+      self.pings = _autotallydict(self.pings, user.name)
     # Character and Word count
     self.chars += len(message.content) # NOTE: CUSTOM EMOJIS AFFECT CHAR COUNT?
     self.words += len(message.content.split())
@@ -67,7 +76,7 @@ class _MsgStatistics:
     for e in getemojis(message.content):
       self.emojis = _autotallydict(self.emojis, e)
     # Times
-    self.hourslots[(message.created_at - timedelta(hours=7)).hour] += 1
+    self.hourslots[(message.created_at - timedelta(hours=7)).hour] += 1 # -7 is PST
   
   def hourgraph(self, fname='bar.png', show=0):
     y_pos = numpy.arange(len(self.hourslots))
@@ -82,6 +91,18 @@ class _MsgStatistics:
   
 class UserStatistics(_MsgStatistics):
   def __init__(self, user):
-    self.user = user
+    self.id = user.id
+    self.name = user.name
     _MsgStatistics.__init__(self)
-    
+  def __repr__(self):
+    return self.name
+  '''
+  ### Benched code for if using the user object is necessary 
+  def __getstate__(self): # object isn't pickleable with user object attribute
+    state = self.__dict__.copy()
+    del state["user"]
+    return state
+  def __setstate__(self, state):
+    self.__dict__.update(state)
+    self.user = None
+  '''
