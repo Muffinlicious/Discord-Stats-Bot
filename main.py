4e08a7d8c2065175189aebdbe485d3fb518f64d1
Muffinlicious <alicornderpy@gmail.com>
2019-07-11 19:37:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 import discord
 import asyncio
+import typing
 from discord.ext import commands
 from statsbot import StatsBot
 import kymcommands as kym
@@ -8,6 +8,27 @@ import kymcommands as kym
 
 bot = StatsBot(command_prefix='!')
 
+def is_statschannel(): # Ensures that stastistics commands can only be invoked in the channel in which the statistics were collected
+    async def predicate(ctx):
+        if ctx.channel.id == bot.channelstats.id:
+          return True
+        else:
+          return False
+    return commands.check(predicate)
+  
+@bot.command()
+@is_statschannel()
+async def stats(ctx, mention: typing.Optional[discord.Member], arg: str = None):
+  if arg:
+    if arg == 'me':
+      await ctx.send(bot.channelstats.get_user_by_id(ctx.author.id).display_info())
+    elif arg == 'channel':
+      await ctx.send(bot.channelstats.display_info())
+    else:
+      await ctx.send('Invalid argument')
+  elif mention:
+    await ctx.send(bot.channelstats.get_user_by_id(mention.id).display_info())
+    
 @bot.command()
 async def ayy(ctx):
   await ctx.send('lmao')
@@ -26,9 +47,11 @@ async def kymbooru(ctx, *tags):
     await ctx.send('No results for "%s"' % ' '.join(tags))
   else:
     await ctx.send('Image OP: **%s**\n%s' % (img['OP'], img['url']))
-
+    
 @bot.command()
 @commands.is_owner()
 async def quit(ctx):
   await bot.logout()
-client.run('token')
+
+if __name__ == '__main__':
+  bot.run('token')
