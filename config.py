52f50f511f29ac7b8b47e3da12ae9118df69d81c
Muffinlicious <alicornderpy@gmail.com>
2019-07-15 14:32:02 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,11 @@
-_ID_LIST = {'MuffinServ' :
-            (({'name':'MuffinServ', 'id':456942258521112586}), ({'name':'memetown', 'id':456942258521112588})),
-           
-           'Mareserv' :
-            (({'name': 'Mareserv', 'id':362076668141109249}), ({'name': 'mareserv-radio-hour', 'id':369558928083648513})),
-           
-           'Proleague Crafters' :
-            (({'name': 'Proleague Crafters', 'id':433103377015242752}), ({'name': 'general', 'id':433103377015242754})),
-           
-           'KYMServ' :
-            (({'name': 'KYMServ', 'id':100770673609150464}), ({'name': 'general', 'id':100770673609150464})),
-           
-           'RoarServ' :
-            (({'name': 'RoarServ', 'id':532603927817093161}), ({'name': '36th-chamber', 'id':532603928471273483})),
+_ID_LIST = {'Server' :
+            (({'name':'Server', 'id':int()}), ({'name':'channel', 'id':int()})),
+           ### Dictionary is set up in this format
            }
 
-_SERVER = 'MuffinServ'
+_SERVER = 'Server'
 
 default_guild = _ID_LIST[_SERVER][0]
 default_channel = _ID_LIST[_SERVER][1]
 
-limit = 200
+limit = 100
