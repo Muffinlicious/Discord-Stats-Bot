af07f5c3101a1669c7b3c7cda5a58d39bc90b660
Muffinlicious <alicornderpy@gmail.com>
2019-08-10 08:46:31 -0700

    Update kymcommands.py

--- kymcommands.py
+++ kymcommands.py
@@ -1,49 +1,49 @@
-from urllib.request import urlopen, HTTPError
-import time
-import random
-import re
+from bs4 import BeautifulSoup
 
-### LOOK INTO USING AIOHTTP
-RE_IMAGE_PAGE = r'\/photos\/\d{1,8}'
-RE_IMAGE_URL = r'https?:\/\/i\.kym\-cdn\.com\/photos\/images\/original\/\d{3}\/\d{3}\/\d{3}\/[^\s"\']+\.(?:jpg|gif|png|jpeg)'
-RE_OP_PROFILE = r'<a href=[\"\']\/users\/.+?[\"\']>(.+?)<\/a>' # username in first capture group
+from datetime import datetime
+import pytz
 
-MAX_IMAGE = 1509400
-
-def get_image(page): #NOTE: Fix issue with banned authors
-  print(page)
-  time.sleep(3) # wait between requests to avoid IP ban
-  with urlopen(page) as info:
-    html = info.read().decode('utf-8')
-  if not re.search(RE_OP_PROFILE, html):
-    img = {'url': re.search(RE_IMAGE_URL, html).group(), 'OP': '[deactivated user]'}
-  else:
-    img = {'url': re.search(RE_IMAGE_URL, html).group(), 'OP': re.search(RE_OP_PROFILE, html).group(1)}
-  return img
-
-def get_random_image():
-  while 1:
-    randompage = "https://knowyourmeme.com/photos/%s" % random.randint(4, MAX_IMAGE)
-    try:
-      img = get_image(randompage)
-    except HTTPError as errmsg:
-      if str(errmsg) == 'HTTP Error 404: Not Found':
-        print("404 " + randompage)
-        continue
-      else:
-        raise HTTPError(errmsg) # Give traceback in the case of non-404 exception
-    else:
-      return img
-
-#https://knowyourmeme.com/search?context=images&q=test+search
-
-def get_image_from_tags(tags):
-  query = 'https://knowyourmeme.com/search?context=images&q=%s' % '+'.join(tags)
-  time.sleep(3)
-  with urlopen(query) as info:
-    html = info.read().decode('utf-8')
-  photos = re.findall(RE_IMAGE_PAGE, html)
-  if not photos: #Return blank dictionary if there are no results
-    return {}
-  return get_image('https://knowyourmeme.com%s' % random.choice(photos))
+def _g(url):
+  from urllib.request import urlopen
+  with urlopen(url) as url:
+    return url.read().decode('utf-8')
   
+def PST_to_UTC(dt):
+  tz = pytz.timezone('US/Pacific')
+  utcdate = tz.normalize(tz.localize(dt).astimezone(pytz.utc))
+  return utcdate
+
+def get_comment(html):
+  soup = BeautifulSoup(html, 'html.parser')
+  article = soup.find('article', {'class':'comment rel thumbable'})
+  
+  content = article.find('div', {'class':'message'})
+  for imgtag in content.find_all('img'): # replaces image tags with links to image
+    link = imgtag['data-src']
+    content.img.replace_with(link)
+    
+  message_dict = {'contents': content.text}
+
+  message_dict['score'] = article.find('div', {'class':'thumb_mini_container'}).text
+  message_dict['date'] = PST_to_UTC(datetime.fromtimestamp(int(article['data-timestamp'])))
+  
+  authorlink = article.find('a', {'class':'photo abs'})
+  
+  if authorlink: # in case the account is 404'd
+    message_dict['account'] = 'http://knowyourmeme.com' + authorlink['href']
+  else:
+    authorlink = article.find('span', {'class':'photo abs'})
+    message_dict['account'] = None
+  authorinfo = authorlink.find('img')
+
+  message_dict['author'] = authorinfo['title']
+  message_dict['avatar'] = authorinfo['data-src']
+
+  message_dict['url'] = 'http://knowyourmeme.com' + article.find('a', {'class':'permalink'})['href']
+  return message_dict
+
+def get_image(html):
+  soup = BeautifulSoup(html, 'html.parser')
+  info = soup.find('div', {'class':'x_brb6 c'})
+  
+
